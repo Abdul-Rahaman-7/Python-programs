8341faacd0b552dd73d7719af4c35f6e1c24b957
Abdul-Rahaman-7 <168440301+Abdul-Rahaman-7@users.noreply.github.com>
2024-09-10 22:58:36 +0530

    Add files via upload

---
+++ product.py
@@ -0,0 +1,10 @@
+# Main code
+# Get user input for the two integers
+A = int(input("Enter the first integer: "))
+B = int(input("Enter the second integer: "))
+
+# Calculate the product
+product = A * B
+
+# Display the result
+print("The product of", A, "and", B, "is", product)
